4505ca9abd768507b73389a4f930f6a07a5b639b
nigelgilbert <ngilbert@smu.edu>
2016-10-06 23:44:30 -0500

    pushing changes off my mac

--- bin/build.blocks.js
+++ bin/build.blocks.js
@@ -2,34 +2,19 @@ module.exports = [
   {
     id: 0,
     name: 'file reader',
-    position: {
-      x: 0,
-      y: 0
-    },
-    view: '',
     logic: 'lib/reader.js',
-    input: []
+    input: null
   },
   {
     id: 1,
     name: 'string modifier',
-    position: {
-      x: 0,
-      y:0
-    },
-    view: '',
     logic: 'lib/mutate.js',
-    input: [0]
+    input: 0
   },
   {
     id: 3,
     name: 'console logger',
-    position: {
-      x: 0,
-      y:0
-    },
-    view: '',
     logic: 'lib/logger.js',
-    input: [1]
+    input: 1
   }
 ];
--- compiler/compiler-v2.js
+++ compiler/compiler-v2.js
@@ -1,4 +1,6 @@
 /**
+Algorithm pseudo-code
+
 1. read in the build.block.js
 2. parse it to .json
 3. map blocks -> # of blocks that use it's output as input
@@ -23,6 +25,77 @@
       - chains.get(0) is the root chain (program entry)
     branches: map<id,object>
       - .json data used to generate branches in build.js
+
+
     NOTE: might be better to store functions and branches by their index in chain array.
 6. generate executable
 */
+"use strict";
+
+const fs = require('fs');
+const path = require('path');
+
+module.exports.compile = function(target) {
+  const blockJson = require(path.join('..', target));
+  const functions = extractFunctions(blockJson);
+  const { chains, branches } = link(blockJson);
+  // generateCompiledCode(functions, run);
+}
+
+function extractFunctions(blockfile) {
+  const urls = blockfile.map(block => block.logic);
+  const functions = urls.map(url => require(path.join('..', url)));
+  return functions;
+}
+
+function link(blockfile) {
+  let chains, branches = null;
+  chains = createBlockExecutionChains(blockfile);
+  return { chains, branches };
+}
+
+// Returns a Map of Arrays of functions.
+// Each array is a sequence of functions that transform data,
+// i.e. the preceeding function pipes it's data to the next.
+function createBlockExecutionChains(blockfile) {
+  const blockOutputMap = createBlockOutputMap(blockfile);
+  const blockStack = [];
+  const chains = [];
+  // assuming for now that the first function is the root (for simplicity)
+  blockStack.push(blockfile[0]);
+
+  const chain = [];
+  while (blockStack.length > 0) {
+    const block = blockStack.pop();
+    const outputs = blockOutputMap.get(block);
+    if (outputs.length > 1) {
+      chain.push(null); // null will represent a branch
+      //
+      // TODO: 
+      // for each output:
+      //     push output onto branch stack (as a new subchain head)
+      //
+    } else if (outputs.length === 1) {
+      chain.push(block);
+    } else {
+
+    }
+  }
+}
+
+// Returns a map which maps a block id to the ids of its outputs.
+// TODO: make this map a block id to the actual blocks, not their ids
+function createBlockOutputMap(blockfile) {
+  const outputstMap = new Map();
+  blockfile.forEach(block => {
+    if (block.input !== null) {
+      const inputFunctionsOutputs = outputsMap.get(block.input);
+      if (inputFunctionsOutputs === 'undefiend') {
+       inputFunctionsOutputs = []; 
+      }
+      inputFunctionsOutputs.push(block.id)
+      outputsMap.set(block.input, frequency++);
+    } 
+  });
+  return outputsMap;
+}
